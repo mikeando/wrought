8830cbbb5823dcc1ff9cfd8cb468e025066c75a6
Michael Anderson <drmikeando@gmail.com>
2024-10-22 17:47:49 +0800

    Initial attempt at the bindings definitions

---
+++ wrought-wasm-bindings/src/lib.rs
@@ -0,0 +1,123 @@
+
+
+use serde::Deserialize;
+use serde::Serialize;
+
+type WroughtResult<T> = Result<T,String>;
+
+#[cfg(not(feature="host"))]
+mod client {
+    use super::*;
+
+// Declare the extern functions that will be provided by the host
+    #[link(wasm_import_module = "env")]
+    extern "C" {
+        fn wrought_write_file(
+            path_ptr: *const u8,
+            path_len: usize,
+            content_ptr: *const u8,
+            content_len: usize,
+        );
+        fn wrought_read_file(
+            path_ptr: *const u8,
+            path_len: usize,
+        );
+        fn wrought_get_metadata(
+            path_ptr: *const u8,
+            path_len: usize,
+            key_ptr: *const u8,
+            key_len: usize,
+        );
+        fn wrought_set_metadata(
+            path_ptr: *const u8,
+            path_len: usize,
+            key_ptr: *const u8,
+            key_len: usize,
+            content_ptr: *const u8,
+            content_len: usize
+        );
+        fn wrought_ai_query(
+            query_ptr: *const u8,
+            query_len: usize,
+        );
+    }
+
+
+    pub struct Wrought {}
+
+    impl Wrought {
+        pub fn write_file(&mut self, path: &Path, value: &[u8]) -> WroughtResult<()> {
+            let path_buf = format!("{}", path.display()).as_bytes();
+            let len = unsafe {
+                wrought_write_file(path_buf.as_ptr(), path_buf.len(), value.as_ptr(), value.len());
+                wasmcb::get_call_buffer_len()
+            };
+            let mut out_buf = vec![0u8; len];
+            unsafe {
+                wasmcb::read_call_buffer(out_buf.as_mut_ptr(), out_buf.len());
+            }
+            serde_json::from_slice(&out_buf).unwrap()
+        }
+
+        pub fn read_file(&mut self, path: &Path) -> WroughtResult<Option<Vec<u8>>> {
+            let path_buf = format!("{}", path.display()).as_bytes();
+            let len = unsafe {
+                wrought_read_file(path_buf.as_ptr(), path_buf.len());
+                wasmcb::get_call_buffer_len()
+            };
+            let mut out_buf = vec![0u8; len];
+            unsafe {
+                wasmcb::read_call_buffer(out_buf.as_mut_ptr(), out_buf.len());
+            }
+            serde_json::from_slice(&out_buf).unwrap()
+        }
+
+        pub fn get_metadata(&mut self, path: &Path, key: &str) -> WroughtResult<Option<String>> {
+            let path_buf = format!("{}", path.display()).as_bytes();
+            let key_buf = key.as_bytes();
+            let len = unsafe {
+                wrought_get_metadata(path_buf.as_ptr(), path_buf.len(), key_buf.as_ptr(), key_buf.len());
+                wasmcb::get_call_buffer_len()
+            };
+            let mut out_buf = vec![0u8; len];
+            unsafe {
+                wasmcb::read_call_buffer(out_buf.as_mut_ptr(), out_buf.len());
+            }
+            serde_json::from_slice(&out_buf).unwrap()
+        }
+
+        pub fn set_metadata(&mut self, path: &Path, key: &str, value: &str) -> WroughtResult<()> {
+            let path_buf = format!("{}", path.display()).as_bytes();
+            let key_buf = key.as_bytes();
+            let value_buf = value.as_bytes();
+            let len = unsafe {
+                wrought_set_metadata(path_buf.as_ptr(), path_buf.len(), key_buf.as_ptr(), key_buf.len(), value_buf.as_ptr(), value_buf.len());
+                wasmcb::get_call_buffer_len()
+            };
+            let mut out_buf = vec![0u8; len];
+            unsafe {
+                wasmcb::read_call_buffer(out_buf.as_mut_ptr(), out_buf.len());
+            }
+            serde_json::from_slice(&out_buf).unwrap()
+        }
+
+        pub fn ai_query(&mut self, query: &str) -> WroughtResult<String> {
+            let query_buf = query.as_str();
+            let len = unsafe {
+                wrought_ai_query(query_buf.as_ptr(), query_buf.len());
+                wasmcb::get_call_buffer_len()
+            };
+            let mut out_buf = vec![0u8; len];
+            unsafe {
+                wasmcb::read_call_buffer(out_buf.as_mut_ptr(), out_buf.len());
+            }
+            serde_json::from_slice(&out_buf).unwrap()
+            todo!();
+        }
+    }
+}
+
+#[cfg(not(feature="host"))]
+pub use client::*;
+
+
